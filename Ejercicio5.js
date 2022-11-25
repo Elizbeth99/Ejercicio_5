f6f037e4dbcaf1f819d11921a1d9f364a337620d
Eli <113862594+Elizbeth99@users.noreply.github.com>
2022-11-24 23:43:41 -0600

    Add files via upload

---
+++ Ejercicio5.js
@@ -0,0 +1,18 @@
+//let AltoCm = 150 
+//let AltoM = 1.50
+
+//let PesoKg = 62
+
+//let AltoRedondeado = mate . techo (AltoM)
+//let PesoRedondeado = mate . piso (PesoKg)
+
+//let Igual = num . MaxValue + 1 === num .ValorMaximo //MaxValue 
+
+const  altura_cm  =  171
+const  altura_m  =  1.71
+const peso_kg =  70.2
+
+const  altura_red  =  Math . ceil ( altura_m )
+const  peso_red  =  Math . floor ( peso_kg )
+
+const  Iguales  =  Number . MAX_VALUE  +  1  ===  Number . MAX_VALUE
